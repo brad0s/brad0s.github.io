5167e86dcc84a0f4d182f9c874e85a1eaf73c727
Braden Wright <brad0swrizzy@gmail.com>
2021-09-28 13:56:34 -1000

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-// custom typefaces
-import "typeface-roboto-mono"
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism-tomorrow.css"
